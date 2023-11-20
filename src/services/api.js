216739e1d8d6ce8e532d21fc48c836a8ea38ee15
Tiago Raupp <tiraupp@gmail.com>
2023-11-20 08:28:47 -0300

    fix: ajuste api deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: "https://lazy-teal-bee-vest.cyclic.app",
-//   timeout: 10000,
-// });
-
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://lazy-teal-bee-vest.cyclic.app",
   timeout: 10000,
 });
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:3000",
+//   timeout: 10000,
+// });
